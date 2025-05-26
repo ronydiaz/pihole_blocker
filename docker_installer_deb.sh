c8e91813e58175729259ac6ca943387d82727232
ronydiaz <ronny.diiazparedes@gmail.com>
2025-05-25 19:01:50 -0600

    Adding docker installer for debian

---
+++ docker_installer_deb.sh
@@ -0,0 +1,18 @@
+# 1. Update Existing Packages
+sudo apt update
+sudo apt upgrade -y
+
+
+# 2. Install Required Packages
+sudo apt install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+
+
+
+
+
+
